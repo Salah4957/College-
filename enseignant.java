3f286e9424864e2b46c3291c71d42b92fa7fe57b
vibiscus <virginie.rieber@gmail.com>
2016-06-27 21:00:03 +0200

    Update enseignant.java

--- enseignant.java
+++ enseignant.java
@@ -1,14 +1,15 @@
 public class enseignant extends personne{
 	int numen;
+	matiere matiere; 
 
-	public enseignant(String nom, String prenom, int numen){
+	public enseignant(String nom, String prenom, int numen, matiere matiere){
 		super(nom, prenom);
 		this.numen=numen;
+		this.matiere=matiere; 
 		}
 	
 	public String toString(){
-		return super.nom+", "+super.prenom+", "+numen;
+		return super.nom+", "+super.prenom+", "+numen+", "+matiere;
 	}
-	
 
 }
