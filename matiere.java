d2aa8c1613eab44efc5c179b3fba14cdc2bb689f
VirgileP <virgile.perton@orange.fr>
2016-07-17 17:45:38 +0200

    Update matiere.java

--- matiere.java
+++ matiere.java
@@ -10,17 +10,4 @@ public class matiere {
 	public String getMatiere(){
 		return this.matiere;
 	}
-	/**Recherche Matière**/
-	public String rechMat(matiere m){
-	String s = "";
-	for (matiere mati : mat){
-		if( mati == m ){
-			s += m;
-		}
-	}
-	if (s != NULL)
-		return s;
-	else
-		return "Aucun résultat trouvé";
-	}
 }
