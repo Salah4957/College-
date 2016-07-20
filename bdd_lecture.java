37c4123c112584dd3a9c471ce6be4665826297dd
VirgileP <virgile.perton@orange.fr>
2016-07-20 22:35:45 +0200

    Update bdd_lecture.java

--- bdd_lecture.java
+++ bdd_lecture.java
@@ -10,7 +10,7 @@ public class bdd_lecture {
 	static final String USER ="root";
 	static final String PWD="";
 
-	public static void main(String[] args) {
+	public static void connLectEleve(int ID) {
 		Connection conn = null;
 		Statement stmt = null;
 		    try {
@@ -21,12 +21,13 @@ public class bdd_lecture {
 		    //Etape 3: Ouverture connexion
 		      System.out.println("Connexion à la BDD...");
 		      conn = DriverManager.getConnection(DB_URL,USER, PWD);
+			  System.out.println("Connexion établie...");
 		      
 		    //Etape 4: Execution de la requête
 		      System.out.println("Création de la requête...");
 		      stmt = conn.createStatement();
 		      String sql;
-		      sql = "SELECT nom, prenom, dateNaissance, adresse, ine, absence FROM eleve";
+		      sql = "(SELECT  ine, nom, prenom, dateNaissance, adresse, absence FROM eleve WHERE ine='"+ID+"')";
 		      ResultSet rs = stmt.executeQuery(sql);
 		      
 		    //Etape 5: Extraction data du ResultSet
@@ -40,11 +41,11 @@ public class bdd_lecture {
 		    	  float absence = rs.getFloat("absence");
 		    	  
 		    	  //affichage des valeurs de la BDD
-		    	  System.out.print("Nom: "+nom);
+				  System.out.print("INE: "+ine);
+		    	  System.out.print(", Nom: "+nom);
 		    	  System.out.print(", Prenom:"+prenom);
 		    	  System.out.print(", Date de Naissance: "+dateNaissance);
 		    	  System.out.print(", adresse: "+adresse);
-		    	  System.out.print(", INE: "+ine);
 		    	  System.out.println(", Absence: "+absence);
 		      }
 		      
@@ -77,6 +78,139 @@ public class bdd_lecture {
 		    		se.printStackTrace();
 		    	}
 		    }
-		    System.out.println("Au revoir!");
+		  }
+
+	public static void connLectProf(int NUM) {
+		Connection conn = null;
+		Statement stmt = null;
+		    try {
+		    //Etape 2: Enregistrement JDBC Driver
+		      Class.forName("com.mysql.jdbc.Driver");
+		      System.out.println("Driver O.K.");
+		      
+		    //Etape 3: Ouverture connexion
+		      System.out.println("Connexion à la BDD...");
+		      conn = DriverManager.getConnection(DB_URL,USER, PWD);
+			  System.out.println("Connexion établie...");
+		      
+		    //Etape 4: Execution de la requête
+		      System.out.println("Création de la requête...");
+		      stmt = conn.createStatement();
+		      String sql;
+		      sql = "(SELECT numen, nom, prenom, dateNaissance, adresse, matiere, nomClasse FROM enseignant WHERE numen='"+NUM+"')";
+		      ResultSet rs = stmt.executeQuery(sql);
+		      
+		    //Etape 5: Extraction data du ResultSet
+		      while(rs.next()){
+		    	  //Retrieve par colonne
+				  int numen = rs.getInt("numen");
+		    	  String nom = rs.getString("nom");
+		    	  String prenom = rs.getString("prenom");
+		    	  String dateNaissance = rs.getString("dateNaissance");
+		    	  String adresse = rs.getString("adresse");
+		    	  String matiere = rs.getString("matiere");
+				  String nomClasse = rs.getString("nomClasse");
+		    	  
+		    	  //affichage des valeurs de la BDD
+				  System.out.print("NUMEN: "+numen);
+		    	  System.out.print(", Nom: "+nom);
+		    	  System.out.print(", Prenom:"+prenom);
+		    	  System.out.print(", Date de Naissance: "+dateNaissance);
+		    	  System.out.print(", adresse: "+adresse);
+		    	  System.out.print(", Matiere: "+matiere);
+				  System.out.println(", Classe: "+nomClasse);				  
+		      }
+		      
+		    //Etape 6: Nettoyage de l'environnement
+		      rs.close();
+		      stmt.close();
+		      conn.close();		         
+		    } 
+		    catch (SQLException se) {
+		    	//Gestion erreurs pour JDBC
+		      se.printStackTrace();
+		    }
+		    catch (Exception e){
+		    	//Gestion erreurs pour Class.forName
+		    	e.printStackTrace();
+		    }
+		    finally{
+		    	//bloc finally utilisé pour fermer les ressources
+		    	try{
+		    		if(stmt != null)
+		    			stmt.close();
+		    	}
+		    	catch (SQLException se2){
+		    	}//rien à faire
+		    	try{
+		    		if(conn != null)
+		    			conn.close();
+		    	}
+		    	catch (SQLException se){
+		    		se.printStackTrace();
+		    	}
+		    }
+		  }
+	
+	public static void connLectClasse(String nClasse) {
+		Connection conn = null;
+		Statement stmt = null;
+		    try {
+		    //Etape 2: Enregistrement JDBC Driver
+		      Class.forName("com.mysql.jdbc.Driver");
+		      System.out.println("Driver O.K.");
+		      
+		    //Etape 3: Ouverture connexion
+		      System.out.println("Connexion à la BDD...");
+		      conn = DriverManager.getConnection(DB_URL,USER, PWD);
+			  System.out.println("Connexion établie...");
+		      
+		    //Etape 4: Execution de la requête
+		      System.out.println("Création de la requête...");
+		      stmt = conn.createStatement();
+		      String sql;
+		      sql = "(SELECT nomClasse, profPrincipal, FROM classe WHERE nomClasse='"+nClasse+"')";
+		      ResultSet rs = stmt.executeQuery(sql);
+		      
+		    //Etape 5: Extraction data du ResultSet
+		      while(rs.next()){
+		    	  //Retrieve par colonne
+		    	  String nomClasse = rs.getString("nomClasse");
+		    	  String profPrincipal = rs.getString("profPrincipal");
+		    	  
+		    	  //affichage des valeurs de la BDD
+				  System.out.print("Classe: "+nomClasse);
+		    	  System.out.println(", Professeur Principal: "+profPrincipal);
+		      }
+		      
+		    //Etape 6: Nettoyage de l'environnement
+		      rs.close();
+		      stmt.close();
+		      conn.close();		         
+		    } 
+		    catch (SQLException se) {
+		    	//Gestion erreurs pour JDBC
+		      se.printStackTrace();
+		    }
+		    catch (Exception e){
+		    	//Gestion erreurs pour Class.forName
+		    	e.printStackTrace();
+		    }
+		    finally{
+		    	//bloc finally utilisé pour fermer les ressources
+		    	try{
+		    		if(stmt != null)
+		    			stmt.close();
+		    	}
+		    	catch (SQLException se2){
+		    	}//rien à faire
+		    	try{
+		    		if(conn != null)
+		    			conn.close();
+		    	}
+		    	catch (SQLException se){
+		    		se.printStackTrace();
+		    	}
+		    }
 		  }
 }
