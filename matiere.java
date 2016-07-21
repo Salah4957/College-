b5a6bffa98f00123830c9ca6ea4d82e44ae9f4cc
vibiscus <virginie.rieber@gmail.com>
2016-07-21 21:07:21 +0200

    Update matiere.java

--- matiere.java
+++ matiere.java
@@ -1,13 +1,25 @@
 public class matiere {
 	String nomMatiere =""; 
 	
+	// constructeur
 	public matiere(String nomMatiere){
 		this.nomMatiere = nomMatiere;
 	}
+	
+	// afficher le nom de la matière
 	public String toString(){
-		return "Nom matière: " +nomMatiere;
+		return nomMatiere + " ";
 	}
-	public String getMatiere(){
+ 
+	// récupérer le nom de la matière
+	public String getNomMat(){
 		return this.nomMatiere;
 	}
+	
+	// modifier le nom de la matière
+	public void setNomMatiere(String nomMatiere) {
+		this.nomMatiere = nomMatiere;
+	}
+	
 }
+
