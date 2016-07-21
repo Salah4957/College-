1552d23e830b068b94b3087797091185d31f83b8
vibiscus <virginie.rieber@gmail.com>
2016-07-21 21:05:49 +0200

    Update and rename MENU.java to menu.java

--- menu.java
+++ menu.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class test {
+public class menu {
 	static 	Scanner scan = new Scanner(System.in);
 	
 	public static void main(String[] args) {
