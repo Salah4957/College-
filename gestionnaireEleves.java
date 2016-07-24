4a6cd2026fb9ced9be69e91a01463c10209a21c8
vibiscus <virginie.rieber@gmail.com>
2016-07-24 14:07:44 +0200

    Update gestionnaireEleves.java

--- gestionnaireEleves.java
+++ gestionnaireEleves.java
@@ -1,39 +1,37 @@
-	import java.util.ArrayList;
+import java.util.ArrayList;
 
 	public class gestionnaireEleves {
 		static ArrayList <eleve> eleves = null;
-		
-		public gestionnaireEleves(){
-			if(eleves==null)
-				eleves = new ArrayList<eleve>();
-		}
-		
-		/*public ArrayList<eleve> getGestionnaire(){			
-			return eleves;
-		}*/
-		
-		public void ajouteEleve(eleve el){
-			eleves.add(el);
-		}
-		
-		public String toString(){
-			String s= ""; 
-			for(eleve el : eleves){
-				s+=el.toString();
-			}
-		    return s;
-		     
+	
+	// création liste eleves si aucun eleve
+	public gestionnaireEleves(){
+	if(eleves==null)
+		eleves = new ArrayList<eleve>();
 	}
 		
-		/**Recherche eleve**/
-		public eleve rechEleve(int i){
-			int j=0;
-			eleve res = null;
-			while (j<eleves.size() && res == null){
-				if(eleves.get(j).ine==i)
-					res = eleves.get(j);
-				j++;
-			}
-			return res;
+	// ajouter un élève
+	public void ajouteEleve(eleve el){
+		eleves.add(el);
+	}
+	
+	// afficher les élèves
+	public String toString(){
+		String s= ""; 
+		for(eleve el : eleves){
+			s+=el.toString();
 		}
+	    return s;   
+	}
+		
+	// recherche eleve
+	public eleve rechEleve(int i){
+		int j=0;
+		eleve res = null;
+		while (j<eleves.size() && res == null){
+			if(eleves.get(j).ine==i)
+				res = eleves.get(j);
+			j++;
+		}
+		return res;
+	}
 }
