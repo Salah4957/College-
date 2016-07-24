fa91f586c8d3ed5a710520dc795d132b0122c161
vibiscus <virginie.rieber@gmail.com>
2016-07-24 14:06:28 +0200

    Update enseignant.java

--- enseignant.java
+++ enseignant.java
@@ -2,37 +2,37 @@ public class enseignant extends personne{
 	int numen;
 	matiere matiere; 
 
+	// constructeur 1
 	public enseignant(String nom, String prenom, int numen, matiere matiere){
 		super(nom, prenom);
 		this.numen=numen;
 		this.matiere=matiere; 
 		}
 	
-	// constructeur 1
-		public enseignant(String nom, String prenom,String adresse, String dateNaissance, int numen, matiere matiere){
-			super(nom, prenom, adresse, dateNaissance);
-			this.numen=numen;
-			this.matiere=matiere;
-		}
+	// constructeur 2
+	public enseignant(String nom, String prenom,String adresse, String dateNaissance, int numen, matiere matiere){
+		super(nom, prenom, adresse, dateNaissance);
+		this.numen=numen;
+		this.matiere=matiere;
+	}
 	
-	//getter
+	// récupérer le numen
 	public int getNumen(){
 		return this.numen;
 	}
 	
+	// récupérer un enseignant
 	public String toString(){
 		return super.nom+" "+super.prenom+", NUMEN: "+numen+", professeur de "+matiere;
 	}
-	//Modifier matiere
-	//getters
+	
+	// récupérer une matiere
 	public matiere getMatiere(){
 		return this.matiere;
-		
 	}
-	//setters
-	public void setMatiere(matiere x){ 
-				
-		matiere = x;     
-									
+	
+	// modifier la matière
+	public void setMatiere(matiere x){ 	
+		matiere = x;     							
 	}
 }
