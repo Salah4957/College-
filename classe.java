656547ae96b390e0224b35b7655a51eb3fb550e1
vibiscus <virginie.rieber@gmail.com>
2016-07-24 14:05:10 +0200

    Update classe.java

--- classe.java
+++ classe.java
@@ -1,6 +1,4 @@
-
 import java.util.ArrayList;
-
 
 public class classe {
 	private ArrayList<eleve> eleves = new ArrayList<eleve>();
@@ -31,27 +29,26 @@ public class classe {
 			this.nomClasse= nomClasse;
 		}
 	
-		
 	public ArrayList<enseignant> getEns() {
 			return ens;
 		}
 
-	// ajout élève --> UTILE?
+	// ajout élève 
 	public void ajoutEleve(eleve a){
 		eleves.add(a);
 	}
 	
-	// ajout Prof --> UTILE?
+	// ajout Prof 
 	public void ajoutEnseignant(enseignant e){
 		ens.add(e);
 	}
 	
-	// affichage 
+	// affichage classe
 	public String toString(){
 		return nomClasse;
 	}
 	
-	//Calculer la moyenne d'une classe : moyenne des moyennes de chaque élève
+	// calculer la moyenne d'une classe : moyenne des moyennes de chaque élève
 	public Double moyenneClasse(){
 		Double somme = 0.0;
 		for (int i=0; i<eleves.size(); i++){
@@ -60,7 +57,7 @@ public class classe {
 		return somme/eleves.size();
 	}
 	
-	//Calculer la moyenne d'une classe dans une matière : moyenne des moyennes de chaque élève
+	// calculer la moyenne d'une classe dans une matière : moyenne des moyennes de chaque élève
 	public Double moyenneClasseMat(matiere m){
 		Double somme = 0.0;
 		for (int i=0; i<eleves.size(); i++){
@@ -70,7 +67,7 @@ public class classe {
 	}
 	
     
-	// Classement moyennes de la classe
+	// classement moyennes de la classe
 	public ArrayList <eleve> classementClasse(){
 		ArrayList <eleve> Eleves = new ArrayList <eleve>();
 		ArrayList <Double> Notes = new ArrayList <Double>();
@@ -85,7 +82,7 @@ public class classe {
 		return Eleves; 
 	}
 	
-	// Classement moyennes de la classe dans une matiere
+	// classement moyennes de la classe dans une matiere
 	public ArrayList <eleve> classementClasseMat(matiere m){
 		ArrayList <eleve> Eleves = new ArrayList <eleve>();
 		ArrayList <Double> Notes = new ArrayList <Double>();
@@ -99,32 +96,38 @@ public class classe {
 		}
 		return Eleves; 
 	}
-	//Modifier classe
-	//getters
+	
+	// récupérer le nom de la classe
 	public String getNomClasse(){
 		return this.nomClasse;
 	}
+	
+	// récupérer le niveau de la classe
 	public int getNiveau(){
 		return this.niveau;
 	}
+	
+	// récupérer le prof principal
 	public String getProfPrincipal(){
 		return this.profPrincipal;
 	}
-	//Setters
+	
+	// modifier le nom de la classe
 	public void setNomClasse(String x){
 		nomClasse = x;
-		
 	}
+	
+	// modifier le niveau de la classe
 	public void setNiveau(int x){
 		niveau = x;
-		
 	}
+	
+	// modifier le prof principal
 	public void setProfPrincipal(String x){
-		profPrincipal = x;
-		
+		profPrincipal = x;	
 	}
 
-	/** Afficher tous les élèves d'une classe **/
+	// afficher tous les élèves d'une classe 
 	public String AffichEleve(){
 		String s = new String();
 		for(eleve el : eleves ){
@@ -133,7 +136,7 @@ public class classe {
 		return s;
 	}
 	
-	/** Afficher tous les enseignants d'une classe **/
+	// afficher tous les enseignants d'une classe 
 	public String AfficheEnseignant(){
 		String s = new String();
 		for(enseignant prof : ens ){
@@ -141,10 +144,10 @@ public class classe {
 		}
 		return s;
 	}
+	
 	//supprimer élève
 	public void supEleve(eleve a){
 		eleves.remove(a);
-		
 	}
 	
 	// recherche élève
@@ -158,4 +161,3 @@ public class classe {
 		return false;
 	}
 }
-
