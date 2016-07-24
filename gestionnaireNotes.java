8495406ba68e6223f636f4e2369ff09ef25f5e34
vibiscus <virginie.rieber@gmail.com>
2016-07-24 14:09:17 +0200

    Update gestionnaireNotes.java

--- gestionnaireNotes.java
+++ gestionnaireNotes.java
@@ -1,15 +1,16 @@
+
 import java.util.ArrayList;
 
 public class gestionnaireNotes {
- 
 	static ArrayList <note> notes = null;
 	
+	// création liste notes si aucune note
 	public gestionnaireNotes(){
 		if(notes==null)
 			notes = new ArrayList<note>();
 	}
 	
-	// récupère liste de toutes les notes d'un élève 
+	// récupèrer liste de toutes les notes d'un élève 
 	public ArrayList <note> recupNotesEleve(eleve e){
 		ArrayList <note> noteEleve = new ArrayList <note>();
 		for (note n: notes){
@@ -20,7 +21,7 @@ public class gestionnaireNotes {
 		return noteEleve;
 	}
 	
-	// récupère liste de toutes les notes d'un élève dans une matière
+	// récupèrer liste de toutes les notes d'un élève dans une matière
 	public ArrayList <note> recupNotesEleveMat(eleve e, matiere m){
 		ArrayList <note> noteEleveMat = new ArrayList <note>();
 		for (note n: notes){
@@ -31,9 +32,8 @@ public class gestionnaireNotes {
 		return noteEleveMat;
 	}
 	
-	
-	// Ajouter une note dans la liste des notes
-		public void ajouter(note n){
-			notes.add(n);
-		}
+	// ajouter une note dans la liste des notes
+	public void ajouter(note n){
+		notes.add(n);
+	}
 }
