0bdf7229d6fc40c71395e81f8c036fda6c3f670d
VirgileP <virgile.perton@orange.fr>
2016-07-26 21:21:26 +0200

    Update enseignant.java

--- enseignant.java
+++ enseignant.java
@@ -2,19 +2,18 @@ public class enseignant extends personne{
 	int numen;
 	matiere matiere; 
 
-	// constructeur 1
+	// constructeur 2
 	public enseignant(String nom, String prenom, int numen, matiere matiere){
 		super(nom, prenom);
 		this.numen=numen;
 		this.matiere=matiere; 
 		}
 	
-	// constructeur 2
-	public enseignant(String nom, String prenom,String adresse, String dateNaissance, int numen, matiere matiere){
-		super(nom, prenom, adresse, dateNaissance);
+	// constructeur 3
+	public enseignant(String nom, String prenom, String dateNaissance, String adresse, classe nomClasse, int numen, matiere matiere){
+		super(nom, prenom, dateNaissance, adresse, nomClasse);
 		this.numen=numen;
 		this.matiere=matiere;
-	}
 	
 	// récupérer le numen
 	public int getNumen(){
